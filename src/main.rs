16a42d7492cd2fcb7a0df07169da443f6196a2d2
Luigi <luigiantonmagdamit@gmail.com>
2025-04-12 12:37:10 -0700

    adjusted error propogations and added cursive

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,31 @@
-use std::{fmt::Error, fs::File};
-use std::io::BufReader;
-use rodio::{Decoder, OutputStream, OutputStreamHandle, PlayError, Sink, StreamError};
+use cursive::views::{Dialog, TextView};
+mod radio;
 
-struct RadioMeta {
-    _stream: OutputStream,
-    stream_handle: OutputStreamHandle,
-    sink: Option<Sink>
-}
-pub enum RadioMetaError {
-    StreamOpenFail,
-    SinkInitFail
-}
-impl RadioMeta {
-    fn new() -> Result<RadioMeta, RadioMetaError>{
-        let open_output_stream = OutputStream::try_default();
-        match open_output_stream {
-            Ok((_stream, stream_handle)) => {
-                let success = RadioMeta {
-                    _stream,
-                    stream_handle,
-                    sink: None
-                };
-                Ok(success)
-            },
-            Err(_) => Err(RadioMetaError::StreamOpenFail)
-        }
-    }
-    fn init_sink(&mut self) -> Result<(), RadioMetaError>{
-        let sink_result = Sink::try_new(&self.stream_handle);
-        match sink_result {
-            Ok(sink_success) => {
-                self.sink = Some(sink_success);
-                Ok(())
-            },
-            Err(_) => Err(RadioMetaError::SinkInitFail)   
-        }
-    }
-}
-struct Radio {
-    metadata: RadioMeta,
-    songs: Vec<Song> // should hold the song names. promise that the string will have lifetime or shorter than a
-}
+use radio::radio::{Radio, Song};
 
-impl Radio {
-    fn new() -> Result<Radio, RadioMetaError> {
-        let mut metadata = RadioMeta::new()?;
-        metadata.init_sink()?;
-
-        Ok(Radio {
-            metadata,
-            songs: Vec::new()
-        })
-    }
-    fn queue_song(&mut self, song: Song) {
-        self.songs.push(song);
-    }
-    fn change_song(&self, index: usize) {
-        let song = self.songs.get(index);
-        match song {
-            Some(s) => {
-                let file = File::open(s.path.clone()).unwrap();
-                let source = Decoder::new(BufReader::new(file)).unwrap();
-                if let Some(sink) = &self.metadata.sink {
-                    sink.append(source);
-                    sink.sleep_until_end();
-                }
-
-            },
-            None => panic!()
-        }
-    //     if let Some(song_success) = &song {
-    //         match &song_success.fd {
-    //             Some(st) => {
-    //                 let source = Decoder::new(BufReader::new(st)).unwrap();
-        
-    //                 if let Some(s) = &self.metadata.sink {
-    //                     // Play the audio
-    //                     s.append(source);
-                
-    //                     // block current call until the end
-    //                     s.sleep_until_end();
-    //                 }
-    //             },
-    //             _ => panic!()
-    //         }
-            
-    //     }
-        
-    }
-}
-struct Song {
-    name: String,
-    path: String,
-
-    
-}
-impl Song {
-    fn new(name: String, path: String) -> Song {
-        Song {name, path }
-    }
-    fn change_name(&mut self, new_name: String) {
-        self.name = new_name;
-    }
-    fn change_path(&mut self, new_path: String) {
-        self.path = new_path;
-    }
-
-        
-}
-pub enum SongError {
-    PathNotFound,
-    FileOpenFailure,
-    Unknown
-}
-fn main() {
+fn init_radio_test() {
     // Create an output stream and a stream handle
     let r  = Radio::new();
-
-    match r {
-        Ok(mut success) => {
-            println!("uhh");
-            let song = Song::new(String::from("test"), String::from("./music/test2.mp3"));
-
-            success.queue_song(song);
-            success.change_song(0);
-
-        }
-        Err(_) => panic!()
+    let song_name = String::from("test");
+    let song_path = String::from("./music/test2.mp3");
+    let song = Song::new(&song_name, &song_path);
+    if let Ok(mut radio) = r {
+        let _ = &radio.queue_song(song);
+        let _ = radio.change_song(0);
     }
-    // let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+}
+fn main() {
+    init_radio_test();
 
-    // // Create a sink (audio output)
-    // let sink = Sink::try_new(&stream_handle).unwrap();
+    
+    // let mut siv = cursive::default();
 
-    // Load the audio file
+    // // Creates a dialog with a single "Quit" button
+    // siv.add_layer(Dialog::around(TextView::new("Hello Dialog!"))
+    //                      .title("Cursive")
+    //                      .button("Quit", |s| s.quit()));
+
+    // // Starts the event loop.
+    // siv.run();
 
 }
---
+++ src/radio/radio.rs
@@ -0,0 +1,111 @@
+use std::{fmt::Error, fs::File};
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, OutputStreamHandle, PlayError, Sink, StreamError};
+
+
+pub struct RadioMeta {
+    _stream: OutputStream,
+    stream_handle: OutputStreamHandle,
+    pub sink: Option<Sink>
+}
+pub enum RadioMetaError {
+    StreamOpenFail,
+    SinkInitFail
+}
+impl RadioMeta {
+    pub fn new() -> Result<RadioMeta, RadioMetaError>{
+        let open_output_stream = OutputStream::try_default();
+        match open_output_stream {
+            Ok((_stream, stream_handle)) => {
+                let success = RadioMeta {
+                    _stream,
+                    stream_handle,
+                    sink: None
+                };
+                Ok(success)
+            },
+            Err(_) => Err(RadioMetaError::StreamOpenFail)
+        }
+    }
+    pub fn init_sink(&mut self) -> Result<(), RadioMetaError>{
+        let sink_result = Sink::try_new(&self.stream_handle);
+        match sink_result {
+            Ok(sink_success) => {
+                self.sink = Some(sink_success);
+                Ok(())
+            },
+            Err(_) => Err(RadioMetaError::SinkInitFail)   
+        }
+    }
+}
+pub struct Radio <'a> {
+    metadata: RadioMeta,
+    songs: Vec<Song<'a>> // should hold the song names. promise that the string will have lifetime or shorter than a
+}
+
+pub enum RadioError {
+    IndexNotFound,
+    InitFailure,
+    SourceFailure
+}
+impl <'a> Radio <'a> {
+    pub fn new() -> Result<Radio<'a> , RadioMetaError> {
+        let mut metadata = RadioMeta::new()?;
+        metadata.init_sink()?;
+
+        Ok(Radio {
+            metadata,
+            songs: Vec::new()
+        })
+    }
+    pub fn queue_song(&mut self, song: Song<'a>) {
+        self.songs.push(song);
+    }
+    
+    pub fn change_song(&self, index: usize) -> Result<(), RadioError> {
+        if let Some(song) = self.songs.get(index) {
+            let source = Radio::create_source(song.path);
+            if let Some(sink) = &self.metadata.sink {
+                sink.append(source?);
+                sink.sleep_until_end();
+            }
+            Ok(())
+        } else {
+            Err(RadioError::IndexNotFound)
+        }
+        
+    }
+    fn create_source(path: &'a str) -> Result<Decoder<BufReader<File>>, RadioError> {
+        let file = File::open(path);
+        if let Ok(file_success) = file {
+            let source = Decoder::new(BufReader::new(file_success)).unwrap();
+            return Ok(source);
+        }
+        Err(RadioError::SourceFailure)
+        
+    }
+}
+pub struct Song <'a> {
+    name: &'a str,
+    path: &'a str,
+
+    
+}
+impl <'a> Song <'a> {
+    pub fn new(name: &'a str, path: &'a str) -> Song <'a> {
+        Song {name, path }
+    }
+    pub fn change_name(&mut self, new_name: &'a str) {
+        self.name = new_name;
+    }
+    pub fn change_path(&mut self, new_path: &'a str) {
+        self.path = new_path;
+    }
+
+        
+}
+pub enum SongError {
+    PathNotFound,
+    FileOpenFailure,
+    Unknown
+}
