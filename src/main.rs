5e83079840b3c1f857cb86b77558a02911167b04
Luigi Magdamit <luigiantonmagdamit@gmail.com>
2025-04-16 12:26:53 -0700

    added versions menu

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,20 @@
 
 mod radio;
 use crate::radio::view::UserInterface;
-use cursive::reexports::ahash::HashMap;
-use cursive::{Cursive, CursiveRunnable};
-use cursive::views::{Button, Dialog, DummyView, LinearLayout, NamedView, ResizedView, SelectView, EditView};
-use cursive::traits::*;
-use cursive::theme::Theme;
-
-use crate::radio::control::on_submit;
+use std::env;
 
 
 use crate::radio::files::{get_name, create_table};
 fn main() {
-    // let mut ui = UserInterface::init();
-    // ui.set_dir("./music");
-    // ui.run();
+    let args: Vec<String> = env::args().collect();
 
-    let table = create_table("./music");
-    
-    let mut s = cursive::default();
-
-    let select = song_menu();
-    s.add_layer(select);
-    for (name, path_list) in table {
-        let name = name.clone();
-        s.call_on_name("select", |v: &mut SelectView<String>| {
-            v.add_item_str(name);
-        });
-    }
-    s.run();
+    let path = &args[1];
+    let table = create_table(&path);
+    let mut ui = UserInterface::init();
+    ui.set_table(table);
+    ui.set_dir("");
+    ui.run();
 
 }
 
-fn song_menu() -> ResizedView<NamedView<SelectView>> {
-    SelectView::<String>::new()
-        .with_name("select")
-        .fixed_size((100, 5))
-}
-
-fn add_name(s: &mut Cursive, table: &mut HashMap<String, Vec<String>>) {
-    for (name, path_list) in table {
-        let name = name.clone();
-        s.call_on_name("select", |v: &mut SelectView<String>| {
-            v.add_item_str(name);
-        });
-    }
-}
 
--- src/radio/view.rs
+++ src/radio/view.rs
@@ -1,30 +1,38 @@
+use std::collections::HashMap;
+use std::path;
+use std::rc::Rc;
+use cursive::reexports::crossbeam_channel::select;
 use cursive::{Cursive, CursiveRunnable};
 use cursive::views::{Button, Dialog, DummyView, LinearLayout, NamedView, ResizedView, SelectView, EditView};
 use cursive::traits::*;
 use cursive::theme::Theme;
-
 use crate::radio::theme::InterfaceTheme;
 use crate::radio::control::on_submit;
 use crate::radio::files::get_files;
 
 pub struct UserInterface<'a> {
     siv: CursiveRunnable,
-    dir: &'a str
+    dir: &'a str,
+    table: HashMap<String, Vec<String>>
 }
 impl<'a>UserInterface <'a>{
     pub fn init() -> Self {
         UserInterface { 
             siv: cursive::default(),
-            dir: ""
+            dir: "",
+            table: HashMap::new()
         }
     }
     pub fn set_dir(&mut self, dir: &'a str) {
         self.dir = dir;
     }
+    pub fn set_table(&mut self, table: HashMap<String, Vec<String>>) {
+        self.table = table;
+    }
     pub fn run(&mut self) {
         self.set_theme(InterfaceTheme::get(InterfaceTheme::Windows98));
         self.build_view();
-        self.init_titles();
+        // self.init_titles(get_files("./music"));
         
         self.siv.run();
     }
@@ -41,8 +49,8 @@ impl<'a>UserInterface <'a>{
             v.add_item_str(name);
         });
     }
-    fn init_titles(&mut self) {
-        let filenames = get_files(self.dir);
+    fn init_titles(&mut self, paths: Vec<String>) {
+        let filenames = paths;
         for name in filenames {
             self.add_title(&name);
         }
@@ -50,19 +58,84 @@ impl<'a>UserInterface <'a>{
     fn select() -> ResizedView<NamedView<SelectView>> {
         SelectView::<String>::new()
             .on_submit(on_submit)
-            .with_name("select")
+            .with_name("select_version")
             .fixed_size((100, 5))
     }
     fn build_view(&mut self) {
         let select = UserInterface::select();
         let buttons = UserInterface::menu_buttons();
 
+        // self.siv.add_layer(
+        //     Dialog::around(LinearLayout::horizontal()
+        //     .child(select)
+        //     .child(DummyView)
+        //     .child(buttons))
+        //     .title("Select a file to play"));
+        let menu = self.song_menu();
         self.siv.add_layer(
+            Dialog::around(LinearLayout::horizontal()
+            .child(menu)
+            .child(DummyView)
+            .child(buttons))
+            .title("Songs")
+            .button("Exit", |s| {
+                s.quit();
+            }));
+        for (name, path_list) in &self.table {
+            println!("{}", name);
+            let name = name.clone();
+            self.siv.call_on_name("select", |v: &mut SelectView<String>| {
+                v.add_item_str(name);
+            }
+        );}
+    }
+    fn select_song(&mut self) -> Dialog {
+        let version_select = UserInterface::select();
+        let menu = Dialog::around(LinearLayout::horizontal()
+            .child(version_select)
+            .child(DummyView)
+            ).title("Select a file to play");
+        menu
+        
+    }
+    fn song_menu(&mut self) -> ResizedView<NamedView<SelectView>> {
+        let table = self.table.clone();
+
+        let menu = SelectView::<String>::new()
+            
+            .on_submit(move |s, name: &str| {
+                let table = &table;
+                let versions = table.get(name).unwrap().clone();
+                s.add_layer(UserInterface::version_menu(name));
+                
+                for version_name in versions {
+                    s.call_on_name("select_version", |v: &mut SelectView<String>| {
+                        v.add_item_str(version_name);
+                    });
+                }
+                
+                
+            })
+            
+            .with_name("select")
+            .fixed_size((100, 5));
+        
+        menu
+    }
+
+    fn version_menu(name: &str) -> Dialog{
+        let select = UserInterface::select();
+        let buttons = UserInterface::menu_buttons();
+
+
             Dialog::around(LinearLayout::horizontal()
             .child(select)
             .child(DummyView)
             .child(buttons))
-            .title("Select a file to play"));
+            .title(name)
+            .button("Back", |s| {
+                s.pop_layer();
+            })
     }
     fn set_theme(&mut self, theme: Theme) {
         self.siv.set_theme(theme);
